8d5d3a926865a03c9691d9e58b69e6f315b0f4a0
AminNouri <aminnoori.mus@gmail.com>
2025-08-13 23:41:33 +0330

    [imp] Context manager

--- Week01_AdvancedPython/context_manager.py
+++ Week01_AdvancedPython/context_manager.py
@@ -1,1 +1,26 @@
 # Placeholder for context_manager.py
+
+
+with open('Week01_AdvancedPython/hello.txt', 'r') as f:
+    print(f.readlines())
+    f.close()
+
+
+class FileManager:
+
+    def __init__(self, filename, mode):
+        self.filename = filename
+        self.mode = mode
+
+    def __enter__(self):
+        self.file = open(self.filename, self.mode)
+        return self.file
+    
+    def __exit__(self, exe_type, exe_vals, tracback):
+        self.file.close()
+
+
+with FileManager("Week01_AdvancedPython/hello.txt", "r") as f:
+    f.readlines()
+
+print(f.closed)
