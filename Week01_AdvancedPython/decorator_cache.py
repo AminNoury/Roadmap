90286428c57fac3b07252bbc27751999926581ed
AminNouri <aminnoori.mus@gmail.com>
2025-08-18 00:26:45 +0330

    Add decorator

--- Week01_AdvancedPython/decorator_cache.py
+++ Week01_AdvancedPython/decorator_cache.py
@@ -1,2 +1,14 @@
 # Placeholder for decorator_cache.py
+def decorator(func):
+    def wraper():
+        print("start function")
+        func()
+        print("ending function")
+    return wraper
 
+
+@decorator
+def test():
+    print("midel")
+
+test()
--- Week01_AdvancedPython/generator.py
+++ Week01_AdvancedPython/generator.py
@@ -9,7 +9,6 @@ gen = generator(100)
 
 
 def main():
-
     for i in range(10):
         print(next(gen))
 
